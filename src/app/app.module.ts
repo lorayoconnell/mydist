3e00615f962ab94c851da4ab30638ebed72f6cff
Loray O'Connell <lorayoconnell@me.com>
2019-09-30 20:32:29 -0700

    created messages component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { MyImageComponent } from './my-image/my-image.component';
 import { MyButtonComponent } from './my-button/my-button.component';
 import { MyServiceService } from './my-service.service';
 import { MessageComponent } from './models/message/message.component';
+import { MessagesComponent } from './models/messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyImageComponent,
     MyButtonComponent,
-    MessageComponent
+    MessageComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
