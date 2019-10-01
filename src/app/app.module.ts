b8150955db3481c5289896f102ac802afd36636b
Loray O'Connell <lorayoconnell@me.com>
2019-10-01 08:15:00 -0700

    added user component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { MyImageComponent } from './my-image/my-image.component';
 import { MyButtonComponent } from './my-button/my-button.component';
-import { MyServiceService } from './my-service.service';
 import { MessageComponent } from './models/message/message.component';
 import { MessagesComponent } from './models/messages/messages.component';
+import { UserComponent } from './models/user/user.component';
+
+import { MyServiceService } from './my-service.service';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,8 @@ import { MessagesComponent } from './models/messages/messages.component';
     MyImageComponent,
     MyButtonComponent,
     MessageComponent,
-    MessagesComponent
+    MessagesComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
